f6562a6e43016b40eb711c5a1c10a4ffcee0f942
Dimaeld <60007382+dimaeld@users.noreply.github.com>
2025-12-02 09:16:52 +0200

    Initial Commit

---
+++ CatCatcher/CatCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatCatcherApp.swift
+//  CatCatcher
+//
+//  Created by dimaeld on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CatCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatCatcherTests/CatCatcherTests.swift
@@ -0,0 +1,16 @@
+//
+//  CatCatcherTests.swift
+//  CatCatcherTests
+//
+//  Created by dimaeld on 02.12.2025.
+//
+
+import Testing
+
+struct CatCatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
